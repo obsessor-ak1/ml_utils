4d66a7f5969a016aeaeead4a485f028ffd5b9ee3
Aayush Kumar <aayushkumarfirst@gmail.com>
2025-04-07 14:15:25 +0530

    Implemented cross-correlation (convolution)

---
+++ ml_utils/basic_utils.py
@@ -0,0 +1,27 @@
+import torch
+
+
+def cross_corr_2d(X: torch.Tensor, kernel: torch.Tensor, in_channeled=False, out_channeled=False):
+    if not in_channeled:
+        kernel = kernel[torch.newaxis, :]
+        # Assumes image is also in shape: XxY
+        X = X[torch.newaxis, :]
+    
+    if not out_channeled:
+        kernel = kernel[torch.newaxis, :]
+
+    kh, kw = kernel.shape[2:]
+    ih, iw = X.shape[1:]
+    outputs = []
+    # For every output channel
+    for k in kernel:
+        output_ts = torch.zeros((
+            ih-kh+1, iw-kw+1
+        ))
+        for i in range(0, ih-kh+1):
+            for j in range(0, iw-kw+1):
+                mult = k * X[:, i:i+kh, j:j+kw]
+                output_ts[i, j] = mult.sum()
+        outputs.append(output_ts)
+    
+    return torch.stack(outputs)
